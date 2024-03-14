dc3d3cbd38da98ca4402f8cbcb8c3a6728c2f241
sairamaj <sairamaj@gmail.com>
2024-03-14 12:54:09 -0700

    step17 with apislice added

---
+++ react/step17_redux_turotial_socalmedia_rti_query/redux-essentials-example-app-master/src/features/posts/PostsList.js
@@ -0,0 +1,59 @@
+import React, {  useMemo } from 'react'
+import { Link } from 'react-router-dom'
+import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo';
+import { ReactionButtons } from './ReactionButtons';
+import { Spinner } from '../../components/Spinner'
+import { useGetPostsQuery } from '../api/apiSlice'
+
+const PostExcerpt = ({ post }) => {
+    return (
+        <article className="post-excerpt">
+            <h3>{post.title}</h3>
+            <div>
+                <PostAuthor userId={post.user} />
+                <TimeAgo timestamp={post.date} />
+            </div>
+            <p className="post-content">{post.content.substring(0, 100)}</p>
+
+            <ReactionButtons post={post} />
+            <Link to={`/posts/${post.id}`} className="button muted-button">
+                View Post
+            </Link>
+        </article>
+    )
+}
+
+export const PostsList = () => {
+
+    const {
+        data: posts = [],
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetPostsQuery()
+
+    const sortedPosts = useMemo(() => {
+        const sortedPosts = posts.slice()
+        // Sort posts in descending chronological order
+        sortedPosts.sort((a, b) => b.date.localeCompare(a.date))
+        return sortedPosts
+    }, [posts])
+
+    let content
+    if (isLoading) {
+        content = <Spinner text="Loading..." />
+    } else if (isSuccess) {
+        content = sortedPosts.map(post => <PostExcerpt key={post.id} post={post} />)
+    } else if (isError) {
+        content = <div>{error.toString()}</div>
+    }
+
+    return (
+        <section className="posts-list">
+            <h2>Posts</h2>
+            {content}
+        </section>
+    )
+}
